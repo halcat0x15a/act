08448b4d174cda2a9d201f0a01ee7ea727739d3f
halcat0x15a <halcat0x15a@halcat0x15a-desktop.(none)>
2011-06-19 00:00:29 +0900

    Directionの追加

---
+++ src/main/scala/baskingcat/act/gameplay/Direction.scala
@@ -0,0 +1,9 @@
+package baskingcat.act.gameplay
+
+sealed abstract class Direction
+
+sealed abstract class Forward extends Direction
+
+sealed abstract class Backward extends Direction
+
+sealed abstract class Unknown extends Direction
